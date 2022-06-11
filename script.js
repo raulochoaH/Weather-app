62b66ce871d1cbd9e1aaf22ebd90541af40562fe
Raul Ochoa <raulochoa3005@gmail.com>
2022-06-11 16:02:57 -0430

    Agregué displayWeather

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let weather = {
-    apikey: "c0fb1dc649e3641531f4e3d246a518be",
-    fetchWeather: function (){
+    "apiKey": "c0fb1dc649e3641531f4e3d246a518be",
+    fetchWeather: function (city){
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=caracas&units=metric&appid=c0fb1dc649e3641531f4e3d246a518be"
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
         )
-        .then((response) => Response.json())
-        .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed " + speed + "km/h";
     }
 }
 
